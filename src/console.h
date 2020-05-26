cf568eb974c0b39c0aecf9a5b29769cba83d62cf
small-pony <2262713487@qq.com>
2020-05-26 20:04:24 +0800

    init

---
+++ src/console.h
@@ -0,0 +1,19 @@
+#pragma once
+constexpr auto RESET = "\033[0m";
+constexpr auto BLACK = "\033[30m" /* Black */;
+constexpr auto RED = "\033[31m" /* Red */;
+constexpr auto GREEN = "\033[32m" /* Green */;
+constexpr auto YELLOW = "\033[33m" /* Yellow */;
+constexpr auto BLUE = "\033[34m" /* Blue */;
+constexpr auto MAGENTA = "\033[35m" /* Magenta */;
+constexpr auto CYAN = "\033[36m" /* Cyan */;
+constexpr auto WHITE = "\033[37m" /* White */;
+constexpr auto LIGHTBLACK = "\033[1m\033[30m" /* Bold Black */;
+constexpr auto LIGHTRED = "\033[1m\033[31m" /* Bold Red */;
+constexpr auto LIGHTGREEN = "\033[1m\033[32m" /* Bold Green */;
+constexpr auto LIGHTYELLOW = "\033[1m\033[33m" /* Bold Yellow */;
+constexpr auto LIGHTBLUE = "\033[1m\033[34m" /* Bold Blue */;
+constexpr auto LIGHTMAGENTA = "\033[1m\033[35m" /* Bold Magenta */;
+constexpr auto LIGHTCYAN = "\033[1m\033[36m" /* Bold Cyan */;
+constexpr auto LIGHTWHITE = "\033[1m\033[37m" /* Bold White */;
+constexpr auto CLEAN = "\033[2J" /* Bold White */;
---
+++ src/helpers/stringHelper.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "../include.h"
+using namespace std;
+extern vector<string> split(const string& str, const string& pattern);
